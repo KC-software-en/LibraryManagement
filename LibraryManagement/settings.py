6888a0c476d6bae9e5d6c6e0209463baef5b9157
KC_software_en <kisha.cairncross@gmail.com>
2025-01-28 18:03:22 +0200

    updated environment variable in settings.py

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+# import os to access environment variables
+import os
+
+# import load_dotenv to load environment variables from .env file
+from dotenv import load_dotenv
+
+########################################################################
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fiy82y=0dhi5g=e3w1ybcn1epmgi03qs@&(*u-@1zg++we700%'
+# Save secret key in .env file
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
