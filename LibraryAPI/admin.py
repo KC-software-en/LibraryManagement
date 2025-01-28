ecde58304bbb1975a77f0070793a78a13781801a
KC_software_en <kisha.cairncross@gmail.com>
2025-01-28 22:33:47 +0200

    added model and registered in admin

--- LibraryAPI/admin.py
+++ LibraryAPI/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+# import models
+from .models import Book
+
+#########################################################
+
 # Register your models here.
+
+# register Book
+admin.site.register(Book)
--- LibraryAPI/models.py
+++ LibraryAPI/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# create a model for a book
+class Book(models.Model):
+    # get the primary key from the database    
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    edition = models.CharField(max_length=13)
+    published_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    summary = models.TextField()
+    # save the time each time a book updates, not just when it was created
+    created_at = models.DateTimeField(auto_now_add=True)
+    # if the book is available use bool
+    availability = models.BooleanField(default=True)
+
+    # return the title
+    def __str__(self):
+        return self.title
