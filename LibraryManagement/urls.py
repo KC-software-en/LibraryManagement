fcc13cb20be98e1f605b62f6a67a3c74d83be7c0
KC_software_en <kisha.cairncross@gmail.com>
2025-01-29 20:12:38 +0200

    added path to LibraryAPI views in LibraryManagement/urls.py

--- LibraryManagement/urls.py
+++ LibraryManagement/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# import include
+from django.urls import path, include
 
+##################################################################
+
+# add a path to the app urls
+# use an empty str to set the homepage of the project
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('LibraryAPI.urls')),
 ]
