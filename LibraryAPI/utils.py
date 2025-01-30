7113e6608a33f2865268ac4d6c4dd1d6698f2fbc
KC_software_en <kisha.cairncross@gmail.com>
2025-01-30 20:33:12 +0200

    added a fetch_rate_limit_info function in LibraryAPI/utils.py

---
+++ LibraryAPI/utils.py
@@ -0,0 +1,55 @@
+# import timezone
+from django.utils import timezone
+
+# import datetime
+from datetime import datetime, timedelta
+
+# import cache
+from django.core.cache import cache
+
+###################################################################
+
+# define a function to retrieve rate limit information
+# use view as the parameter to access throttle class configured for that view
+# - to generate the correct cach key
+# - & maintain consistency with DRF's internal patterns
+def fetch_rate_limit_info(request, view): 
+    # get 1st throttle class instance from the view's throttle_classes list
+    throttle = view.throttle_classes[0]()
+
+    # check if throttle has a rate attribute (i.e. 100/hour) else none was configured
+    if not hasattr(throttle, 'rate'):
+        return None
+    
+    # parse rate limit details
+    num_of_requests, duration = throttle.parse_rate(throttle.rate)
+
+    # get the current user's request history
+    key = throttle.get_cache_key(request, view)
+    history = cache.get(key, [])
+
+    # get the current timestamp
+    now = timezone.now()
+
+    # clean old history
+    while history and history[-1] <= now - timedelta(seconds=duration):
+        history.pop()
+
+    # add current request timestamp to history
+    history.insert(0, now)
+
+    # save updated history to cache
+    cache.set(key, history, duration)
+
+    # calculate remaining requests
+    remaining_requests = max(num_of_requests - len(history), 0)
+
+    # calculate reset time
+    reset_time = int((now + timedelta(seconds=duration)).timestamp())
+
+    # return the rate limit information 
+    return {
+        "X-RateLimit-Limit": num_of_requests,
+        "X-RateLimit-Remaining": remaining_requests,
+        "X-RateLimit-Reset": reset_time
+    }
