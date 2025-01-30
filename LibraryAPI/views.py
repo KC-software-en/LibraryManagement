b0c7d3e6c265ebfcf4b5d3619384bcb53da426b9
KC_software_en <kisha.cairncross@gmail.com>
2025-01-30 20:30:31 +0200

    added error response for nonexistant book & rate headers in LibraryAPI/views.py

--- LibraryAPI/views.py
+++ LibraryAPI/views.py
@@ -15,6 +15,15 @@ from .models import Book
 # import serializers
 from .serializers import BookSerializer
 
+# import utils functions
+from .utils import fetch_rate_limit_info
+
+# import NotFound for book endpoints that do not exist
+from rest_framework.exceptions import NotFound
+
+# import error for when a book ID endpoint does not exist
+from django.http import Http404
+
 ########################################################################
 
 # Create your views here.
@@ -29,6 +38,17 @@ class BookViewSet(ModelViewSet):
     # specify the serializer class for this viewset
     serializer_class = BookSerializer
 
+    # override the get_object to customise 404 handling
+    # - it enables error response formatting in the retrieve() method
+    def get_object(self):
+        try:
+            # try to get the object using parent class's get_object method
+            return super().get_object()
+        
+        except Http404:
+            # raise django's exception if the object was not found
+            raise NotFound()    
+
     # override the list method to handle GET requests with a custom message
     def list(self, request, *args, **kwargs):
         # get the queryset of all Book objects
@@ -36,12 +56,14 @@ class BookViewSet(ModelViewSet):
 
         # check if queryset is empty and provide a message
         # use 200 status instead of 204 since there was a successful request & a message in the Response
+        # call utils function to fetch rate limit info
         if not queryset.exists():
             return Response(
                 {"status": "success",
                 "code": 200,
                 "message": "There are no Books in the library",
-                "data": []
+                "data": [],
+                "headers": fetch_rate_limit_info(request, self),
                 },
                 status=status.HTTP_200_OK,
                 )
@@ -55,26 +77,46 @@ class BookViewSet(ModelViewSet):
                 "code": 200,
                 "message": "Successfully retrieved all Books",
                 "data": serializer.data,
+                "headers": fetch_rate_limit_info(request, self),
             },
             status=status.HTTP_200_OK,
             )
     
     # override the retrieve method to handle GET requests with a custom message
     def retrieve(self, request, *args, **kwargs):
-        # get the book instance to be retrieved
-        instance = self.get_object()
-        # serialize the book instance using the serializer class
-        serializer = self.get_serializer(instance)
-        # return a response with the serialized data
-        return Response(
-            {
-                "status": "success",
-                "code": 200,
-                "message": "Successfully retrieved Book",
-                "data": serializer.data,
-            },
-            status=status.HTTP_200_OK,
-            )
+        # use defensive programming with try-except blocks
+        try:
+            # get the book instance to be retrieved
+            instance = self.get_object()
+            # serialize the book instance using the serializer class
+            serializer = self.get_serializer(instance)
+
+            # return a response with the serialized data
+            # call utils function to fetch rate limit info
+            return Response(
+                {
+                    "status": "success",
+                    "code": 200,
+                    "message": "Successfully retrieved Book",
+                    "data": serializer.data,
+                    "headers": fetch_rate_limit_info(request, self),
+                },
+                status=status.HTTP_200_OK,
+                )
+        
+        except NotFound:
+            # return a response with a message if the book does not exist
+            # call utils function to fetch rate limit info
+            return Response(
+                {
+                    "status": "error",
+                    "code": 404,
+                    "message": "No Book matches the given query",
+                    "data": None,
+                    "headers": fetch_rate_limit_info(request, self),
+                },
+                status=status.HTTP_404_NOT_FOUND,
+                )
     
     # override the create method to handle POST requests with a custom message
     def create(self, request, *args, **kwargs):
@@ -89,12 +131,16 @@ class BookViewSet(ModelViewSet):
         self.perform_create(serializer)
         # return a response with the serialized data and a 201 status code
         headers = self.get_success_headers(serializer.data)
+
+        # return a response with a message if the book was created
+        # call utils function to fetch rate limit info
         return Response(
             {
                 "status": "success",
                 "code": 201,
                 "message": "Successfully added Book",
                 "data": serializer.data,
+                "headers": fetch_rate_limit_info(request, self),
             },            
             status=status.HTTP_201_CREATED, 
             # return headers since the generic modelviewset is was overwritten
@@ -120,13 +166,16 @@ class BookViewSet(ModelViewSet):
         self.perform_update(serializer)
         # return a response with the serialized data and a 201 status code
         headers = self.get_success_headers(serializer.data)
+        
         # return a response with the serialized data
+        # call utils function to fetch rate limit info
         return Response(
             {
                 "status": "success",
                 "code": 200,
                 "message": "Successfully updated Book",
                 "data": serializer.data,
+                "headers": fetch_rate_limit_info(request, self),
             }, 
             status=status.HTTP_200_OK,
             # return headers since the generic modelviewset is was overwritten
@@ -140,12 +189,15 @@ class BookViewSet(ModelViewSet):
         instance = self.get_object()
         # delete the book object from the database
         self.perform_destroy(instance)
+        
         # return a response with a 204 status code
+        # call utils function to fetch rate limit info
         return Response(
             {
                 "status": "success",
                 "code": 204,
                 "message": "Successfully deleted Book",
+                "headers": fetch_rate_limit_info(request, self),
             },
             status=status.HTTP_204_NO_CONTENT,
             )
