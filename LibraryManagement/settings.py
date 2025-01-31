c051eaa9dbb8ba74f848ea9c7a105bad8efae0e0
KC_software_en <kisha.cairncross@gmail.com>
2025-01-31 16:13:00 +0200

    added URL for PythonAnyWhere in ALLOWED_HOSTS in LibraryManagement/settings.py

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+# add the URLs for local host and PythonAnyWhere
+ALLOWED_HOSTS = ["localhost", "KCswe.pythonanywhere.com"]
 
 
 # Application definition
